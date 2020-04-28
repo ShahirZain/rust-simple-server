3c4ac82b20375fa85a0d3d45f143b6116ed4d12c
shahirZain <shahirzain100@gmail.com>
2020-04-28 16:42:29 +0500

    build simple server for get & post request

--- simple-server/src/main.rs
+++ simple-server/src/main.rs
@@ -3,16 +3,21 @@ use simple_server::{Server, StatusCode};
 
 fn main() {
    let servers = Server::new(|request , mut response|{
-       println!("request {} {}",request.method(),request.uri());
+
+        let body = String::from_utf8(request.body().to_vec()).unwrap();
+       println!("request {} {} body {}",request.method(),request.uri(),body);
 
        match(request.uri().path(),request.method().as_str()){
            ("/","GET") =>{
                Ok(response.body(String::from("Hello from get request").into_bytes())?)
            },
            ("/form","POST") => {
-               Ok(response.body(String::from("Hello from post request ").into_bytes())?)
+               Ok(response.body(body.into_bytes())?)
            },
-           (_,_) => Ok(response.body(String::from("invalid reuqest").into_bytes())?)
+           (_,_) => {
+               response.status(StatusCode::NOT_FOUND);
+               Ok(response.body(String::from("invalid reuqest").into_bytes())?)
+            }
        }
    });
    servers.listen("127.0.0.1","5000");
